3fd219d27eb85b4fb6bfdc13052d43c0deff5240
Jim Veneskey <github@strifecrafter.com>
2015-08-16 15:11:28 +0000

    starting to implement Ajax mapping

---
+++ json_example.php
@@ -0,0 +1,23 @@
+<?php
+//request data from the database
+//code here to connect to database and get the data you want
+
+/* Example JSON format
+{
+  "item1": "Battleship",
+  "item2": "100,25",
+  "item3": "12 knots"
+}
+*/
+
+$item1 = "Battleship";
+$item2 = "100, 25";
+$item3 = "12 knots";
+
+//return in JSON format
+echo "{";
+echo "item1: ", json_encode($item1), "n";
+echo "item2: ", json_encode($item2), "n";
+echo "item3: ", json_encode($item3), "n";
+echo "}";
+?>
